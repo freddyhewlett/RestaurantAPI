3e04a5f9fb9629c8ef1dedf2fc51feeef4e09adc
Federico <freddyhewlett@gmail.com>
2022-11-11 15:26:19 -0300

    first commit

---
+++ Services/Mango.Services.ProductAPI/Repository/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using Mango.Services.ProductAPI.Models.DTO;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductDto>> GetProducts();
+        Task<ProductDto> GetProductById(Guid id);
+        Task<ProductDto> CreateUpdateProduct(ProductDto name);
+        Task<bool> DeleteProduct(Guid name);
+    }
+}
---
+++ Services/Mango.Services.ProductAPI/Repository/ProductRepository.cs
@@ -0,0 +1,42 @@
+﻿using AutoMapper;
+using Mango.Services.ProductAPI.DbContexts;
+using Mango.Services.ProductAPI.Models;
+using Mango.Services.ProductAPI.Models.DTO;
+using Microsoft.EntityFrameworkCore;
+
+namespace Mango.Services.ProductAPI.Repository
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly ApplicationDbContext _context;
+        private IMapper _mapper;
+
+        public ProductRepository(ApplicationDbContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Task<ProductDto> CreateUpdateProduct(ProductDto name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> DeleteProduct(Guid name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<ProductDto> GetProductById(Guid id)
+        {
+            Product product = await _context.Products.Where(x => x.ProductId == id).FirstOrDefaultAsync();
+            return _mapper.Map<ProductDto>(product);
+        }
+
+        public async Task<IEnumerable<ProductDto>> GetProducts()
+        {
+            IEnumerable<Product> productList = await _context.Products.ToListAsync();
+            return _mapper.Map<List<ProductDto>>(productList);
+        }
+    }
+}
