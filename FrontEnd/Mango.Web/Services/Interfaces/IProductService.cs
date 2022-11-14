8a7e5ea6eb668a310967afd9fc99480167013d6d
Federico <freddyhewlett@gmail.com>
2022-11-14 09:49:09 -0300

    services Mango.Web

---
+++ FrontEnd/Mango.Web/Services/Interfaces/IProductService.cs
@@ -0,0 +1,13 @@
+﻿using Mango.Web.Models;
+
+namespace Mango.Web.Services.Interfaces
+{
+    public interface IProductService
+    {
+        Task<T> GetAllProductsAsync<T>();
+        Task<T> GetProductByIdAsync<T>(Guid id);
+        Task<T> CreateProductAsync<T>(ProductDto productDto);
+        Task<T> UpdateProductAsync<T>(ProductDto productDto);
+        Task<T> DeleteProductAsync<T>(Guid id);
+    }
+}
