e3a6e6f99a8ab6fbe1577dc85d6205c44bc219a8
Federico <freddyhewlett@gmail.com>
2022-11-17 14:58:59 -0300

    cart and models dtos

---
+++ Services/Mango.Services.ShoppingCartAPI/Models/Dto/CartDetailsDto.cs
@@ -0,0 +1,12 @@
+﻿namespace Mango.Services.ShoppingCartAPI.Models.Dto
+{
+    public class CartDetailsDto
+    {
+        public Guid CartDetailsId { get; set; }
+        public Guid CartHeaderId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Count { get; set; }
+        public virtual CartHeaderDto CartHeader { get; set; }
+        public virtual ProductDto Product { get; set; }
+    }
+}
