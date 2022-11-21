26a9fefe18e96e0506aa9313b834972783612e43
Federico <freddyhewlett@gmail.com>
2022-11-21 18:00:25 -0300

    coupon code logic

---
+++ FrontEnd/Mango.Web/Models/Dto/CouponDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Mango.Web.Models.Dto
+{
+    public class CouponDto
+    {
+        public Guid CouponId { get; set; }
+        public string CouponCode { get; set; }
+        public double DiscountAmount { get; set; }
+    }
+}
