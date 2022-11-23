2a393cb28108ac5900ae8ce9c4f366309b985ccd
Federico <freddyhewlett@gmail.com>
2022-11-23 09:38:06 -0300

    order API + db config + program config + repositories

---
+++ Services/Mango.Services.OrderAPI/Models/OrderDetails.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Mango.Services.OrderAPI.Models
+{
+    public class OrderDetails
+    {
+        [Key]
+        public Guid OrderDetailsId { get; set; }
+        [ForeignKey("OrderHeaderId")]
+        public Guid OrderHeaderId { get; set; }
+        public Guid ProductId { get; set; }
+        public int Count { get; set; }
+        public string ProductName { get; set; }
+        public double Price { get; set; }
+        public virtual OrderHeader OrderHeader { get; set; }
+    }
+}
---
+++ Services/Mango.Services.OrderAPI/Repositories/OrderRepository.cs
@@ -0,0 +1,6 @@
+﻿namespace Mango.Services.OrderAPI.Repositories
+{
+    public class OrderRepository
+    {
+    }
+}
